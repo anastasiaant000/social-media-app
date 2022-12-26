f5f0fc355136f4c66a98b5522c1c129dfe135ad2
Anastasia Antoniou <76211868+anastasiaant000@users.noreply.github.com>
2022-12-26 14:02:43 +0200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,20 @@
+package org.example;
 
+import org.example.views.LoginSignupFrame;
+import org.example.views.WindowPanel;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new WindowPanel().setVisible(true);
+            }
+        });
+    }
+}
