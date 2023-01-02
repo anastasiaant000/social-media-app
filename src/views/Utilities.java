8431d0f5e89d10135733c8cd20a81c9296f9a837
Anastasia Antoniou <76211868+anastasiaant000@users.noreply.github.com>
2023-01-02 14:07:27 +0200

    Delete Utilities.java

--- src/views/Utilities.java
+++
@@ -1,9 +0,0 @@
-package views;
-
-import javax.swing.*;
-
-public class Utilities {
-    public static void infoBox(String infoMessage, String titleBar) {
-        JOptionPane.showMessageDialog(null, infoMessage, "Message: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
-    }
-}
