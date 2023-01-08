71717494befb010dd571b5bed5a2595d65b138c3
Maria Kampanou <101667048+karoto24@users.noreply.github.com>
2023-01-08 12:28:06 +0200

    Update WindowPanel.java

---
+++ src/views/WindowPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class WindowPanel extends JFrame {
+
+    public WindowPanel() {
+
+        setTitle("SN-AUEB");
+        add(new LoginSignupFrame(this));
+        setMinimumSize(new Dimension(700, 900));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
