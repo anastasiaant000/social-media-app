98559865cda0ac54532328062401b5038c3a9ddb
Maria Kampanou <101667048+karoto24@users.noreply.github.com>
2023-01-11 11:39:51 +0200

    Update MainScreenFrame.java

---
+++ src/views/MainScreenFrame.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainScreenFrame extends JPanel {
+    private JTabbedPane tabbedPane;
+    private JPanel profileTab, postsTab,messagesTab;
+
+    public MainScreenFrame(Profile profile, JFrame window) {
+
+        setLayout(new GridLayout(1, 1));
+
+        // add components to Profile here
+        profileTab = new JPanel();
+        profileTab.add(new UserProfilePanel(profile,this,window));
+        profileTab.setLayout(new GridBagLayout());
+
+        // add components to Posts here
+        postsTab = new JPanel();
+        postsTab.add(new AllPostPanel(profile));
+        postsTab.setLayout(new GridBagLayout());
+
+        // add components to Messages here
+        messagesTab = new JPanel();
+        messagesTab.add(new MessageListPanel(Profile.getUsersUsernames(profile.getUsername()),profile));
+        messagesTab.setLayout(new GridBagLayout());
+
+        tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Profile", profileTab);
+        tabbedPane.addTab("Posts", postsTab);
+        tabbedPane.addTab("Messages", messagesTab);
+
+        add(tabbedPane);
+
+
+    }
+}
